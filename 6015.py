ed59e351a13c25169156c05328d0de56aab5dff2
min0201ji <ymcm2468@gmail.com>
2021-12-26 04:43:05 +0900

    정수 2개 입력받아 그대로 출력하기2

---
+++ 6015.py
@@ -0,0 +1,19 @@
+﻿a, b = input().split()
+print(a)
+print(b)
+
+
+# WA1
+# a, b = split(input())
+# print(a)
+# print(b)
+
+# WA2
+# a, b = int(input()).split()
+# print(a)
+# print(b)
+
+# WA3
+# a, b = (input.split())
+# print(a)
+# print(b)
