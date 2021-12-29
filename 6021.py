9aae4516f667c46c750f1b7744fe4c84e4c5c0eb
min0201ji <ymcm2468@gmail.com>
2021-12-30 00:29:51 +0900

    단어 1개 입력 받아 나누어 출력하기

---
+++ 6021.py
@@ -0,0 +1,18 @@
+﻿s = input()
+print(s[0])
+print(s[1])
+print(s[2])
+print(s[3])
+print(s[4])
+
+# WA
+# s = input().split()
+# print(s[0:5],sep'\n')
+
+# WA2
+# s = input()
+# print(s[0])
+# print=(s[1])
+# print=(s[2])
+# print=(s[3])
+# print=(s[4])
