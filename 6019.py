791e5f0b43179c16363c391cc67e263f623d9ae4
min0201ji <ymcm2468@gmail.com>
2021-12-30 00:24:56 +0900

    주민번호 입력받아 형태 바꿔 출력하기

---
+++ 6019.py
@@ -0,0 +1,34 @@
+﻿y,m,d =input().split('.')
+print(d,m,y,sep='-')
+
+# WA1
+# y,m,d = input().split(-)
+# print(d,m,y)
+
+# WA2
+# y,m,d = input().split('-')
+# print(d,m,y)
+
+# WA3
+# y,m,d =input().split('-')
+# print(d-m-y)
+
+# WA4
+# y =input().split('-')
+# m =input().split('-')
+# d =input().split('-')
+# print(d,m,y)
+
+# WA5
+# y,m,d =input().split('-')
+# print(d,m,y.sep('-'))
+
+# WA5
+# y,m,d =input().split('-')
+# print(d,m,y.sep='-')
+
+# WA6
+# y,m,d =input().split('.')
+# print(d,m,y.sep='-')
+
+# , 과 . 를 꼭 확인하자..
---
+++ 6020.py
@@ -0,0 +1,2 @@
+﻿id_front, id_back = input().split('-')
+print(id_front,id_back,sep='')
